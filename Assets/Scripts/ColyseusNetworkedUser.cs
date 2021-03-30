c3d9754b54ed747730638d652315e9507dfda352
Dr. Burton <drburton@burtonsmediagroup.com>
2021-03-30 15:22:36 -0500

    Add files via upload

---
+++ Assets/Scripts/ColyseusNetworkedUser.cs
@@ -0,0 +1,26 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 1.0.9
+// 
+
+using Colyseus.Schema;
+
+public partial class ColyseusNetworkedUser : Schema {
+	[Type(0, "string")]
+	public string id = default(string);
+
+	[Type(1, "string")]
+	public string sessionId = default(string);
+
+	[Type(2, "boolean")]
+	public bool connected = default(bool);
+
+	[Type(3, "number")]
+	public float timestamp = default(float);
+
+	[Type(4, "map", typeof(MapSchema<string>), "string")]
+	public MapSchema<string> attributes = new MapSchema<string>();
+}
+
